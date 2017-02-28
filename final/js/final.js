3e974f70f63c3e25a91f0f29820b8f7431c52d21
AntonioCampa <antonio_cs@outlook.com>
2017-02-27 19:03:46 -0600

    final

--- final/js/final.js
+++ final/js/final.js
@@ -7,11 +7,38 @@ var titles = $.ajax({
   dataType: "json",
   success: function success(data) {
     console.log(data);
+    runGrid(data);
   },
   error: function error() {
     console.log("error");
   }
 });
+
+function runGrid(titles) {
+  var template = "",
+      $node = $("#listShows"),
+      ajaxTemplate = $.ajax({
+    url: "views/templateGrid/template.html",
+    cache: false,
+    type: "get",
+    dataType: "html",
+    success: function success(data) {
+      template = data;
+    }
+  });
+
+  $.when(ajaxTemplate).done(function () {
+    for (var title = 0; title < titles.length; title++) {
+      var genres = "";
+      for (var i = 0; i < titles[title].genres.length; i++) {
+        genres += titles[title].genres[i] + ", ";
+      }
+      ajaxTemplate = template;
+      ajaxTemplate = ajaxTemplate.replace("##title##", titles[title].name).replace("##score##", titles[title].rating.average).replace("##cover##", titles[title].image.medium).replace("##genres##", genres);
+      $node.append(ajaxTemplate);
+    }
+  });
+}
 
 function runApp() {
   console.log("app runing");
